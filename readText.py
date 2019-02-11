127bf4fc3ef99da4589b581db6f63667da1ef76c
Matthew M-B <matthewmacraebovell@bell.net>
2019-02-11 00:26:12 -0500

    ok

--- readText.py
+++ readText.py
@@ -1,3 +1,10 @@
 def returnText(name):
-    text = open(name,"r").readlines()
+    #file = open("demoText.txt", "w")
+    #file.write(name)
+    text = open("demoText.txt","r").readlines()
+    print(text)
     return (text)
+
+def returnNotes(notes):
+    print(notes)
+    return notes
